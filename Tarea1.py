08afecd34cf31b3cfd5ca423dffe9dd55372349c
Karina Cruz Vázquez <147003518+karycruz99@users.noreply.github.com>
2023-10-09 22:24:51 -0600

    Programación en Python

---
+++ Tarea1.py
@@ -0,0 +1,47 @@
+
+#EJERCICIO 1: Realizar un algoritmo que sume dos números
+
+import math 
+
+a= float (input ("Dame el valor de a="))
+
+b= float (input ("Dame el valor de b="))
+
+print (a +b)
+
+
+
+#EJERCICIO 2: Realizar un algoritmo que permita leer dos valores distintos,
+# determinar cuál de los dos valores es el mayor y escribirlo
+
+c= float (input ("Dame el valor de c="))
+d= float (input ("Dame el valor de d="))
+
+if (c>d):
+    print ("El número mayor es", c)
+
+elif (d>c):
+    print ("el número mayor es", d)
+
+else:
+    print ("En este caso los dos numeros son iguales")
+
+
+
+#EJERCICIO 3: Realizar un algoritmo que permita leer dos valores distintos,
+# determinar cuál de los dos valores es el menor y escribirlo
+
+e= float (input ("Dame el valor de e="))
+f= float (input ("Dame el valor de f="))
+
+if (e<f):
+    print ("El número menor es", e)
+
+elif (f<e):
+    print ("el número menor es", f)
+
+else:
+    print ("En este caso los dos numeros son iguales")
+
+
+    
